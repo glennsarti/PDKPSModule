597a7ebfd852efe2deb5cc59d24358a835fc6522
Glenn Sarti <glennsarti@users.noreply.github.com>
2019-07-16 11:40:27 +0800

    (maint) Updated for PDK 1.11

--- tryit.bat
+++ tryit.bat
@@ -1,11 +1,18 @@
 SETLOCAL
 
-SET pscmd="Import-Module %~dp0PuppetDevelopmentKitV2.psm1; Get-command -Module PuppetDevelopmentKitV2; Test-PDKUnit2"
-REM SET pscmd="Import-Module %~dp0out/PuppetDevelopmentKit.psd1; Get-command -Module PuppetDevelopmentKit"
+ECHO Generating...
+powershell.exe -NoProfile -File generate_module.ps1
+
+REM SET pscmd="Import-Module %~dp0PuppetDevelopmentKitV2.psm1; Get-command -Module PuppetDevelopmentKitV2; Test-PDKUnit2"
+SET pscmd="Set-Location 'C:\Source\PDKPSModule\tmp\testmodule'; Import-Module %~dp0out/PuppetDevelopmentKit.psd1; Get-command -Module PuppetDevelopmentKit; Test-PDKUnit2"
 mkdir tmp
 
-pushd tmp\testmodule
-REM pushd C:\Source\puppetlabs-registry\registry
-REM pwsh -NoExit -Command %pscmd%
-pwsh -Command %pscmd%
-popd
+
+
+REM pushd tmp\testmodule
+REM REM pushd C:\Source\puppetlabs-registry\registry
+REM REM pwsh -NoExit -Command %pscmd%
+pwsh -NoExit -Command %pscmd%
+REM popd
+
+ECHO EXITED TEST BATCH
